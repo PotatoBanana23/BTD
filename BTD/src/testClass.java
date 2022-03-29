bb3b2971a53cf9cbee772dcd7fc3a465d41cdc80
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 <1856059@01-19714.pusd.dom>
2022-03-29 09:09:09 -0700

    testing 2nd commit

--- BTD/src/testClass.java
+++ BTD/src/testClass.java
@@ -1,12 +1,9 @@
 
-public class Runner {
+public class testClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	 
-		System.out.println("This is TestBranch1"); 
-		
-	}	 
+	}
 
 }
